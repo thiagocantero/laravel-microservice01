13fd5839617ae832f16ce1766b2d5c30d9e34e29
thiagocantero <thiagocantero@gmail.com>
2024-04-01 15:50:33 -0300

    feat search with criteria, based on url variable

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -47,12 +47,14 @@ class CategoryController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int  $url
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($url)
     {
-        //
+        $category = $this->repository->where('url', $url)->firstOrFail();
+
+        return new CategoryResource($category);
     }
 
     /**
